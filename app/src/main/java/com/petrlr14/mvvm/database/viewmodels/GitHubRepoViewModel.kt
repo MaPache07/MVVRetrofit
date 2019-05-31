a360c8bf40a15adb899962f46e18b5d71458b23e
MaPache07 <00170517@uca.edu.sv>
2019-05-31 14:29:56 -0600

    Se termino :v

--- app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
+++ app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
@@ -1,11 +1,14 @@
 package com.petrlr14.mvvm.database.viewmodels
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.petrlr14.mvvm.database.RoomDB
 import com.petrlr14.mvvm.database.entities.GitHubRepo
 import com.petrlr14.mvvm.database.repositories.GitHubRepoRepository
+import kotlinx.coroutines.launch
 
 class GitHubRepoViewModel(private val app: Application) : AndroidViewModel(app) {
 
@@ -20,6 +23,22 @@ class GitHubRepoViewModel(private val app: Application) : AndroidViewModel(app) 
 
     fun getAll():LiveData<List<GitHubRepo>>{
         return repository.getAll()
+    }
+
+    fun getRepoAsync(user : String) = viewModelScope.launch {
+        this@GitHubRepoViewModel.nuke()
+        val response = repository.getRepoAsync(user).await()
+        if(response.isSuccessful) with(response){
+            this.body()?.forEach {
+                this@GitHubRepoViewModel.insert(it)
+            }
+        } else with(response){
+            when(this.code()){
+                404 -> {
+                    Toast.makeText(app, "Usuario no encontrado", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     private suspend fun nuke()= repository.nuke()
